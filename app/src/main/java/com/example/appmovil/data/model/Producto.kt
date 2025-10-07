3de06963e0ade6da99fbe8beaf1bd13b94db3022
PedroSaquic <psaquicc2@miumg.edu.gt>
2025-10-06 19:16:46 -0600

    Promt de Modelos

--- app/src/main/java/com/example/appmovil/data/model/Producto.kt
+++ app/src/main/java/com/example/appmovil/data/model/Producto.kt
@@ -1,8 +1,15 @@
 package com.example.appmovil.data.model
 
 data class Producto(
-    val id: Long,
+    val id: Long?, //o genera el servidor al crearse
     val nombre: String,
     val precio: Double,
-    val descripcion: String
+    val categoria: String?, //puede omitirse
+    val creadoEn: String? //lo asigna el backed automaticamente
+)
+
+data class ProductoRequest(
+    val nombre: String,
+    val precio: Double,
+    val categoria: String?
 )
